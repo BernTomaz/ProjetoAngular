82c63dae9530c5d718046d52e573169487052cf1
Bernardo Tomaz <99764547+BernTomaz@users.noreply.github.com>
2025-09-11 15:24:01 -0300

    feat: create user model and explain about FormBuilder

--- src/app/demos/reactiveforms/cadastro/cadastro.component.ts
+++ src/app/demos/reactiveforms/cadastro/cadastro.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { RouterModule  } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Usuario } from './models/usuario';
 
 @Component({
   selector: 'app-cadastro',
@@ -20,7 +21,7 @@ export class CadastroComponent implements OnInit {
 
 
   cadastroForm: FormGroup;
-  
+  usuario: Usuario;
 
   constructor(private fb: FormBuilder) {}
 
@@ -36,7 +37,7 @@ export class CadastroComponent implements OnInit {
   }
 
   adicionarUsuario(){
-    let x = this.cadastroForm.value;    
+        this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
   }
 
 }
